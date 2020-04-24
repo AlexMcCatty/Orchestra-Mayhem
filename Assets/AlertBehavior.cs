2c67a75cfd1c66a61de5684bceda7e1e3bcc3b95
AlexMcCatty <55056536+AlexMcCatty@users.noreply.github.com>
2020-04-23 23:59:13 -0400

    added stuff

---
+++ Assets/AlertBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertBehavior : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject violin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.FindGameObjectWithTag("MainCamera");
+        Transform parent = this.violin.transform;
+        this.transform.LookAt(this.player.transform);
+        this.transform.position = parent.position;
+        this.transform.position = new Vector3(this.transform.position.x - 0.3f, this.transform.position.y + 0.4f, this.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(this.player.transform);
+    }
+}
---
+++ Assets/ViolinController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViolinController : MonoBehaviour
+{
+    public char key;
+    public int secondsToDroop;
+    Animator an;
+    KeyCode realKey;
+    private bool notDrooped;
+    public GameObject alert;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.notDrooped = true;
+        this.an = GetComponent<Animator>();
+        this.an.SetInteger("AnimState", 0);
+        switch (this.key)
+        {
+            case 'q':
+                this.realKey = KeyCode.Q;
+                break;
+            case 'w':
+                this.realKey = KeyCode.W;
+                break;
+            case 'e':
+                this.realKey = KeyCode.E;
+                break;
+            case 'r':
+                this.realKey = KeyCode.R;
+                break;
+            default:
+                break;
+        }
+        this.alert.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.secondsToDroop >= 0 && Time.time >= this.secondsToDroop && this.notDrooped)
+        {
+            this.an.SetInteger("AnimState", 1);
+            this.notDrooped = false;
+            this.alert.SetActive(true);
+        }
+        if(Input.GetKeyDown(this.realKey) || (Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") > 0.2f))
+        {
+            this.an.SetInteger("AnimState", 0);
+            this.alert.SetActive(false);
+        }
+    }
+}
